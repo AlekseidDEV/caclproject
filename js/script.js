4a469da76eae0b063ffffd977753980266d9573d
Alex <lesha.rudiko@gmail.com>
2024-02-27 10:34:10 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const title = document.getElementsByTagName('h1')
+const title = document.getElementsByTagName('h1')[0]
 const btns = document.getElementsByClassName('handler_btn')
 const btnPlus = document.querySelector('.screen-btn')
 const percentItem = document.querySelectorAll('.other-items.percent')
